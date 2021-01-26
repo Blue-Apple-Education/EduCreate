597dac53f95ec44c561cae7e995a221b0fc22b51
stefanfmeyer <stefanfmeyer@gmail.com>
2021-01-26 11:56:41 +0000

    PageBuilder Twig Setup

---
+++ template-page-builder.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * *
+ * Template Name: Page Builder
+ * Description: EduCreate Template for: ACF PRO Flexible Content Page Builder
+ *
+ * @package Wordpress
+ * @subpackage Timber
+ * @since 2021
+ *
+ */
+
+$context = Timber::get_context();
+$post = new TimberPost();
+$context['post'] = $post;
+
+Timber::render('pages/page-page-builder.twig', $context);
